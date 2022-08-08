3939994a5176932b351446ac51c198385355c510
sosiskaki11er <93476714+sosiskaki11er@users.noreply.github.com>
2022-08-08 22:36:00 +0500

    feat:function added

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+        let lettersArr=expr.split('');
+    let morseLet=[];
+    let bitLet=[];
+    let result='';
+    for(let i=0;i<lettersArr.length;i++){
+        morseLet[i]=Object.keys(MORSE_TABLE).find(key=>MORSE_TABLE[key]===lettersArr[i]);
+    }
+    for(let j=0;j<morseLet.length;j++){
+        for(let k=0;k<morseLet[j].length;k++){
+            if(morseLet[j][k]==='.'){
+            bitLet[j]+='10'
+            }
+            if(morseLet[j][k]==='-'){
+            bitLet[j]+='11'
+            }
+        }
+    }
+    for(let i=0;i<bitLet.length;i++){
+        bitLet[i]=bitLet[i].slice(9)
+        while(bitLet[i].length<10){
+            bitLet[i]='0'+bitLet[i]
+            }
+    }
+    for(let k=0;k<bitLet.length;k++){
+        result+=bitLet[k]
+    }
+    return result
 }
 
 module.exports = {
